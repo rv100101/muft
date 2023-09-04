eb003dabc8699c774d6bce212a6e762ca5c503f6
Loven Angelo <lovenangelo.dev@gmail.com>
2023-09-04 13:12:09 +0800

    chore: installed shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App";
 import logo from "@/assets/logo.svg";
+import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <img src={logo} alt="muffin-logo" />
+      <nav className="flex items-center justify-between">
+        <img src={logo} alt="muffin-logo" />
+        <div className="space-x-8">
+          <Button variant={"ghost"}>Link One</Button>
+          <Button variant={"ghost"}>Link Two</Button>
+          <Button variant={"ghost"}>Link Three</Button>
+          <Button>Sign in</Button>
+        </div>
+      </nav>
     </>
   );
 }
