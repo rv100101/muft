51866b56fca28d0829fd3b4f0ff1aaf8e6edcd16
Loven Angelo <lovenangelo.dev@gmail.com>
2023-09-04 15:03:47 +0800

    feat: benefits ui

---
+++ src/components/benefits.tsx
@@ -0,0 +1,17 @@
+const Benefits = () => {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-8">
+      <p className="font-semibold text-4xl text-secondary text-center">
+        Short headline about the benefit of using your product or service
+      </p>
+      <p className="text-center text-secondary text-sm px-32">
+        Highlight the Unique Selling Proposition (USP) with a short summary of
+        the main feature and how it benefits customers. The idea here is to keep
+        it short and direct. If the visitor wishes to learn more they will hit
+        the button.
+      </p>
+    </div>
+  );
+};
+
+export default Benefits;
