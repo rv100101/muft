9e29a837f16d99f8899187b24e365d58673b8352
Loven Angelo <lovenangelo.dev@gmail.com>
2023-09-05 09:15:19 +0800

    feat: responsive footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,10 @@ import { Button } from "./ui/button";
 import { Facebook, Instagram, Twitter, Linkedin } from "lucide-react";
 const Footer = () => {
   return (
-    <div className="mx-[200px] py-32 space-y-4">
+    <div className="mx-8 md:mx-[200px] py-8 md:py-32 space-y-4 flex flex-col">
       <img src={LogoWhite} alt="white muffin logo" />
-      <nav className="flex justify-between">
-        <div className="flex space-x-4">
+      <nav className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between">
+        <div className="flex flex-wrap space-x-2 md:space-x-4">
           <Button className="p-0 font-light text-white" variant={"ghost"}>
             Link One
           </Button>
@@ -23,7 +23,7 @@ const Footer = () => {
             Link Five
           </Button>
         </div>
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 justify-evenly items-center">
           <a href="/" color="white" className="text-white">
             <Facebook />
           </a>
@@ -40,8 +40,8 @@ const Footer = () => {
       </nav>
       <hr />
       <div className="flex justify-between text-white">
-        <p>© 2023 Relume. All rights reserved.</p>
-        <ul className="flex list-none space-x-4 underline">
+        <p className="hidden md:block">© 2023 Relume. All rights reserved.</p>
+        <ul className="flex list-none space-x-4 underline text-xs md:text-md">
           <li>
             <a href="/">Privacy Policy</a>
           </li>
@@ -53,6 +53,9 @@ const Footer = () => {
           </li>
         </ul>
       </div>
+      <p className="text-white text-xs text-center md:hidden block">
+        © 2023 Relume. All rights reserved.
+      </p>
     </div>
   );
 };
