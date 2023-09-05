ec09a127b8c84050849dcadb4d79d16857b0468a
Loven Angelo <lovenangelo.dev@gmail.com>
2023-09-05 15:53:44 +0800

    refactor: changed image

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -3,7 +3,11 @@ import FeatureIcone1 from "@/assets/feature-icon-1.svg";
 import FeatureIcone2 from "@/assets/feature-icon-2.svg";
 import FeatureIcone3 from "@/assets/feature-icon-3.svg";
 import FeatureIcone4 from "@/assets/feature-icon-4.svg";
-import FeaturesPhone from "@/assets/features-phone.svg";
+import PinkBg from "@/assets/features_section/pink-bg.svg";
+import Phone from "@/assets/features_section/phone.png";
+import BottomNav from "@/assets/features_section/bottom-nav.png";
+import AddToStory from "@/assets/features_section/add-to-story.png";
+
 import { motion } from "framer-motion";
 const Features = () => {
   return (
@@ -14,7 +18,7 @@ const Features = () => {
       viewport={{ once: true }}
       className="mt-12"
     >
-      <div className="grid md:grid-cols-3 gap-4 h-max w-full">
+      <div className="grid md:grid-cols-3 gap-4 md:gap-12 h-max w-full">
         <div className="space-y-8 flex flex-col justify-start">
           <FeatureCard
             title="Describe feature one"
@@ -27,8 +31,20 @@ const Features = () => {
             img={FeatureIcone2}
           />
         </div>
-        <div className="md:flex hidden">
-          <img src={FeaturesPhone} alt="features phone image" />
+        <div className="md:justify-center md:items-center relative md:flex hidden">
+          {/* <img src={FeaturesPhone} alt="features phone image" /> */}
+          <img src={PinkBg} alt="pink-vector" />
+          <img className="absolute z-20" src={Phone} alt="phone" />
+          <img
+            className="absolute z-30 -translate-x-24 w-48 -translate-y-20"
+            src={BottomNav}
+            alt="Bottom nav phone image"
+          />
+          <img
+            className="absolute z-30 translate-x-24 w-48 translate-y-12"
+            src={AddToStory}
+            alt="Add to story image"
+          />
         </div>
         <div className="space-y-8 flex flex-col justify-end">
           <FeatureCard
