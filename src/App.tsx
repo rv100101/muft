17b9899838c3704191900c1755788c26d1875c67
Loven Angelo <lovenangelo.dev@gmail.com>
2023-09-08 11:58:42 +0800

    refactor: create landing page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
 import "./App";
-import Hero from "@/components/hero";
-import NavBar from "@/components/navbar";
-import Benefits from "./components/benefits";
-import Features from "./components/features";
-import Cta from "./components/cta";
-import Footer from "./components/footer";
-import GetApp from "./components/getApp";
-import { motion } from "framer-motion";
+import LandingPage from "./pages/landingPage";
 function App() {
   return (
     <>
-      <GetApp />
-      <div className="mx-8 md:mx-12 lg:mx-36">
-        <NavBar />
-        <Hero />
-        <Benefits />
-        <Features />
-        {/* <Testimonials /> */}
-      </div>
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        whileInView={{
-          opacity: 1,
-        }}
-        transition={{
-          delay: 0.5,
-        }}
-        viewport={{ once: true }}
-        className="bg-[#FF7AAF]"
-      >
-        <Cta />
-      </motion.div>
-      <div className="bg-[#0C1223]">
-        <Footer />
-      </div>
+      <LandingPage />
     </>
   );
 }
---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,44 @@
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import NavBar from "@/components/navbar";
+import Benefits from "@/components/benefits";
+import Features from "@/components/features";
+import Cta from "@/components/cta";
+import GetApp from "@/components/getApp";
+import { motion } from "framer-motion";
+
+const LandingPage = () => {
+  return (
+    <>
+      {" "}
+      <GetApp />
+      <div className="mx-8 md:mx-12 lg:mx-36">
+        <NavBar />
+        <Hero />
+        <Benefits />
+        <Features />
+        {/* <Testimonials /> */}
+      </div>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+        }}
+        transition={{
+          delay: 0.5,
+        }}
+        viewport={{ once: true }}
+        className="bg-[#FF7AAF]"
+      >
+        <Cta />
+      </motion.div>
+      <div className="bg-[#0C1223]">
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
