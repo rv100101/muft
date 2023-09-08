e7ca9fb8e461f66ca2e0d9001dd24d173e192bf6
Loven Angelo <lovenangelo.dev@gmail.com>
2023-09-08 14:35:46 +0800

    refactor: responsive

--- src/pages/privacyPolicyPage.tsx
+++ src/pages/privacyPolicyPage.tsx
@@ -10,8 +10,10 @@ const PrivacyPolicyPage = () => {
     </p>
   ));
   return (
-    <div className="space-y-8 my-8 py-8 px-6 rounded-2xl bg-secondaryBackground">
-      <h1 className="text-4xl font-bold text-[#1B2950]">Privacy Policy</h1>
+    <div className="space-y-4 md:space-y-8 md:my-8 py-8 px-6 rounded-2xl bg-secondaryBackground">
+      <h1 className="text-2xl md:text-4xl font-bold text-[#1B2950]">
+        Privacy Policy
+      </h1>
       <div className="text-[#212529] space-y-4">{policies}</div>
     </div>
   );
