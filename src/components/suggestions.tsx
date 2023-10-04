8e991b4bdf4f799239782dcf4466c8bb01d7ad88
Loven Angelo <lovenangelo.dev@gmail.com>
2023-10-04 08:38:45 +0800

    refactor: responsive notification module

--- src/components/suggestions.tsx
+++ src/components/suggestions.tsx
@@ -7,7 +7,7 @@ import FooterLinks from "./footerLinks";
 const Suggestions = () => {
   const suggestions = suggestionsData.map((suggestion, index) => {
     return (
-      <li key={index} className="h-48 w-full relative">
+      <li key={index} className="h-36 lg:h-48 w-full relative">
         <img
           className="h-full w-full rounded-xl z-10"
           src={suggestion.backgroundImage}
@@ -15,13 +15,15 @@ const Suggestions = () => {
         />
         <div className="absolute flex bottom-4 left-4 z-20 space-x-2 items-end">
           <img
-            className="rounded-full h-20 ring-2 ring-primary"
+            className="rounded-full h-12 lg:h-20 ring-2 ring-primary"
             src={suggestion.avatar}
             alt="user avatar"
           />
           <div>
-            <p className="font-semibold">{suggestion.name}</p>
-            <p>{suggestion.country}</p>
+            <p className="font-semibold text-xs lg:text-md">
+              {suggestion.name}
+            </p>
+            <p className="text-xs lg:text-md">{suggestion.country}</p>
           </div>
         </div>
       </li>
@@ -29,10 +31,10 @@ const Suggestions = () => {
   });
 
   return (
-    <div className="w-full h-full p-4 flex flex-col">
+    <div className="w-full h-full pt-4 px-2 lg:p-4 sm:flex flex-col hidden">
       <SearchInput />
       <div className="h-max w-full flex justify-between mt-4 items-center">
-        <p className="font-semibold text-xl">SUGGESTIONS</p>
+        <p className="font-semibold lg:text-xl">SUGGESTIONS</p>
         <div className="flex">
           <Button variant={"ghost"} className="p-0">
             <ChevronLeftIcon />
@@ -42,7 +44,7 @@ const Suggestions = () => {
           </Button>
         </div>
       </div>
-      <ul className="border-top h-full p-2 space-y-4 overflow-y-scroll">
+      <ul className="border-top h-full p-2 space-y-2 lg:space-y-4 overflow-y-scroll">
         {suggestions}
       </ul>
       <div className="w-full border-t pt-2">
