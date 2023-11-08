c264464c044c1a0e0a3610439ba97f9bb0fcc70d
lovenangelo <lovenangelo.dev@gmail.com>
2023-11-08 14:08:00 +0800

    chore: hard code env

--- src/queries/axios.ts
+++ src/queries/axios.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const axiosQuery = axios.create({
   baseURL: "https://muffinfunction.azurewebsites.net/api/",
   headers: {
-    "x-functions-key": import.meta.env.VITE_AZURE_FUNCTIONS_KEY,
-    Authorization: import.meta.env.VITE_TEMP_AUTHORIZATION_TOKEN,
+    "x-functions-key": "nw7phMvGe08_TmVh6DClQF8kytMiHLOQ8ta-hxXQOq9mAzFujxiSbA==",
+    Authorization: "0DB31DEE22DC4C03AD7DAAA9C29518FF3C08D931992A4A5CB0A4FF4CF4707DC6",
   },
 });
 
