c7d06ff5bb41970d86cb2f64edc22d0f6546bff9
Roy Johairi Lucman <roy@syntacticsinc.com>
2023-11-23 16:02:35 +0800

    fix: UI updates

--- src/components/auth/sendResetPin.tsx
+++ src/components/auth/sendResetPin.tsx
@@ -1,4 +1,3 @@
-import { MailIcon } from "lucide-react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { DialogClose } from "@radix-ui/react-dialog";
@@ -10,8 +9,10 @@ import passwordResetQuery from "@/queries/password_reset";
 import { toast } from "../ui/use-toast";
 import { usePasswordResetState } from "@/zustand/auth/passwordReset";
 const SendResetPin = () => {
-  const changePasswordResetState = usePasswordResetState((state)=> state.changeState);
-  const setEmail = usePasswordResetState(state => state.setEmail);
+  const changePasswordResetState = usePasswordResetState(
+    (state) => state.changeState
+  );
+  const setEmail = usePasswordResetState((state) => state.setEmail);
   const emailForm = useFormik({
     initialValues: {
       email: "",
@@ -26,32 +27,29 @@ const SendResetPin = () => {
     },
   });
 
-  const sendResetPinMutation = useMutation(
-    {
-      mutationFn: passwordResetQuery.getPasswordPin,
-      onSuccess: (res) => {
-        console.log(res);
-        if (typeof res.data == 'string') {
+  const sendResetPinMutation = useMutation({
+    mutationFn: passwordResetQuery.getPasswordPin,
+    onSuccess: (res) => {
+      console.log(res);
+      if (typeof res.data == "string") {
+        toast({
+          variant: "destructive",
+          title: "Please try again",
+          description: "Invalid email",
+        });
+        return;
+      } else {
+        if (res.data !== "") {
+          changePasswordResetState("VERIFY");
+          setEmail(res.data.email_address);
           toast({
-            variant: 'destructive',
-            title: 'Please try again',
-            description: 'Invalid email'
+            title: "Password reset pin sent to your email",
+            description: "Check your email for the pin to change your password",
           });
-          return; 
-        }else{
-          if (res.data !== '') {
-            changePasswordResetState("VERIFY");
-            setEmail(res.data.email_address);
-            toast({
-              title: 'Password reset pin sent to your email',
-              description: "Check your email for the pin to change your password"
-            })
-          }
         }
-
-      },
+      }
     },
-  );
+  });
   return (
     <form onSubmit={emailForm.handleSubmit}>
       <div className="space-y-4">
@@ -59,23 +57,22 @@ const SendResetPin = () => {
         <hr />
         <p>Please enter your email to proceed.</p>
         <div className="flex items-center space-x-2 justify-around border-2 px-4 rounded-full">
-          <label htmlFor="email">Email</label>
-          <MailIcon />
+          {/* <label htmlFor="email">Email</label>
+          <MailIcon /> */}
           <Input
             {...emailForm.getFieldProps("email")}
             id="email"
             name="email"
             type="text"
-            placeholder="example@email.com"
+            placeholder="Email Address"
             className="border-none focus-visible:ring-offset-0 focus-visible:ring-0"
           />
         </div>
-        {emailForm.errors.email &&
-          (
-            <div className="error text-red-500 text-sm ml-2">
-              {emailForm.errors.email}
-            </div>
-          )}
+        {emailForm.errors.email && (
+          <div className="error text-red-500 text-sm ml-2">
+            {emailForm.errors.email}
+          </div>
+        )}
         <hr />
         <div className="flex space-x-2 justify-end">
           <DialogClose className="hover: border border-primary rounded-md px-2 py-1 text-sm">
@@ -83,9 +80,7 @@ const SendResetPin = () => {
           </DialogClose>
           <Button
             disabled={sendResetPinMutation.isLoading}
-            className={cn(
-              "text-white w-max rounded-md hover:bg-[#FF599B]/90",
-            )}
+            className={cn("text-white w-max rounded-md hover:bg-[#FF599B]/90")}
             type="submit"
           >
             Reset
@@ -93,6 +88,7 @@ const SendResetPin = () => {
         </div>
       </div>
     </form>
-  )};
+  );
+};
 
 export default SendResetPin;
