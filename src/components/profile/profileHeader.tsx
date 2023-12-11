fbe52e0e77a8545fb45b9c89c34cb63555f63588
lovenangelo <lovenangelo.dev@gmail.com>
2023-12-11 11:39:20 +0800

    fix: profile modal showing error

--- src/components/profile/profileHeader.tsx
+++ src/components/profile/profileHeader.tsx
@@ -13,8 +13,6 @@ import { FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { useFormContext } from "react-hook-form";
-import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
-import ActivateAccount from "@/pages/authenticatedPages/accountActivationPage";
 import uploadQueries from "@/queries/uploading";
 import { toast } from "../ui/use-toast";
 import messagingQuery from "@/queries/messaging";
@@ -154,227 +152,212 @@ const ProfileHeader = ({ userId }: { userId: string }) => {
   };
 
   return (
-    <Dialog open={!user!.is_active}>
-      <DialogContent className="sm:max-w-[425px]">
-        <ActivateAccount />
-      </DialogContent>
-      <div className="items-start p-5 border-b w-full">
-        <div className="flex justify-start items-start space-x-2">
-          {
-            <div className="flex flex-col justify-center items-center space-y-2">
+    <div className="items-start p-5 border-b w-full">
+      <div className="flex justify-start items-start space-x-2">
+        {
+          <div className="flex flex-col justify-center items-center space-y-2">
+            <Button
+              variant={"ghost"}
+              disabled={!isEditing || isUploading}
+              type="button"
+              onMouseOver={() => {
+                if (isEditing) setShowCamera(true);
+              }}
+              onMouseOut={() => {
+                if (isEditing) setShowCamera(false);
+              }}
+              className="relative disabled:opacity-100 h-full w-40 bg-transparent py-0 px-0"
+              onClick={handleGalleryUpload}
+            >
+              {showCamera && <CameraIcon className="absolute" fill="pink" />}
+              {isUploading &&
+                <Loader2 className="absolute animate-spin text-primary" />}
+              <img
+                className={`select-none object-cover h-32 w-32 overflow-clip border-4 border-primary rounded-full`}
+                src={selectedFile ? selectedFile : getImagePath(
+                  headerValues.gallery_uuid,
+                  headerValues.gender,
+                  headerValues.member_uuid?.toString(),
+                )}
+                alt="no image selected"
+              />
+              <input
+                type="file"
+                name="profilePhoto"
+                onChange={handleFileChange}
+                className="invisible w-0 h-0"
+                ref={fileInputRef}
+              />
+            </Button>
+            {isEditing && selectedFile && (
               <Button
-                variant={"ghost"}
-                disabled={!isEditing || isUploading}
+                disabled={isUploading}
+                onClick={handleProfilePhotoUpload}
                 type="button"
-                onMouseOver={() => {
-                  if (isEditing) setShowCamera(true);
-                }}
-                onMouseOut={() => {
-                  if (isEditing) setShowCamera(false);
-                }}
-                className="relative disabled:opacity-100 h-full w-40 bg-transparent py-0 px-0"
-                onClick={handleGalleryUpload}
+                className="hover:bg-[#FF8AB3]/95 w-36 text-xs"
               >
-                {showCamera && <CameraIcon className="absolute" fill="pink" />}
-                {isUploading &&
-                  <Loader2 className="absolute animate-spin text-primary" />}
-                <img
-                  className={`select-none object-cover h-32 w-32 overflow-clip border-4 border-primary rounded-full`}
-                  src={selectedFile ? selectedFile : getImagePath(
-                    headerValues.gallery_uuid,
-                    headerValues.gender,
-                    headerValues.member_uuid?.toString(),
-                  )}
-                  alt="no image selected"
-                />
-                <input
-                  type="file"
-                  name="profilePhoto"
-                  onChange={handleFileChange}
-                  className="invisible w-0 h-0"
-                  ref={fileInputRef}
-                />
+                Update Photo
               </Button>
-              {isEditing && selectedFile && (
-                <Button
-                  disabled={isUploading}
-                  onClick={handleProfilePhotoUpload}
-                  type="button"
-                  className="hover:bg-[#FF8AB3]/95 w-36 text-xs"
+            )}
+          </div>
+        }
+        <div className="w-full">
+          <div className="flex w-full justify-between">
+            <div className="w-42">
+              {!isEditing && (
+                <div
+                  className={`flex flex-row space-x-5 ${
+                    !user!.is_active ? "pt-5 pl-3" : ""
+                  }`}
                 >
-                  Update Photo
-                </Button>
-              )}
-            </div>
-          }
-          <div className="w-full">
-            <div className="flex w-full justify-between">
-              <div className="w-42">
-                {!isEditing && (
-                  <div
-                    className={`flex flex-row space-x-5 ${
-                      !user!.is_active ? "pt-5 pl-3" : ""
-                    }`}
-                  >
-                    <div>
-                      <p className="font-semibold text-[#171717] text-lg ">
-                        {headerValues.nickname}
-                      </p>
-                      {!isEditing && (
-                        <p
-                          className={`text-[#727272] text-sm`}
-                        >
-                          @{`${headerValues.nickname?.toLowerCase()}`}
-                        </p>
-                      )}
-                    </div>
-                    {userId !== user!.member_id.toString() && (
-                      <Button
-                        disabled={!selectedConversation}
-                        type="button"
-                        className="text-xs border-primary hover:bg-primary px-2 py-1"
+                  <div>
+                    <p className="font-semibold text-[#171717] text-lg ">
+                      {headerValues.nickname}
+                    </p>
+                    {!isEditing && (
+                      <p
+                        className={`text-[#727272] text-sm`}
                       >
-                        <Link className="flex" href="/messages">
-                          <p className="flex">
-                            Chat
-                            <span>
-                              <MessageCircleIcon className="h-4" />
-                            </span>
-                          </p>
-                        </Link>
-                      </Button>
-                    )}
-                    {!user!.is_active && (
-                      <DialogTrigger>
-                        <button
-                          type="button"
-                          className="rounded-full bg-green-300 px-4 py-2 text-xs text-green-600 border hover:text-white hover:bg-green-500"
-                        >
-                          Activate Account
-                        </button>
-                      </DialogTrigger>
+                        @{`${headerValues.nickname?.toLowerCase()}`}
+                      </p>
                     )}
                   </div>
-                )}
-                {isEditing && (
-                  <FormField
-                    name="nickname"
-                    render={({ field }) => {
-                      return (
-                        <FormItem>
-                          <FormLabel className="text-primary">
-                            Nickname
-                          </FormLabel>
-                          <Input
-                            placeholder="Enter nickname"
-                            type="text"
-                            defaultValue={field.value}
-                            onChange={field.onChange}
-                            className="outline-0 border border rounded-lg w-full py-3 px-5"
-                            name="nickname"
-                          />
-                          <FormMessage />
-                        </FormItem>
-                      );
-                    }}
-                  />
-                )}
-              </div>
-              {userId === user!.member_id.toString() && (
-                <>
-                  {isEditing && (
-                    <div className="flex space-x-2">
-                      <Button
-                        onClick={!formState.isDirty ? () => {} : () => {
-                          // if (isEditing && !formState.isValid) {
-                          //   toast({
-                          //     variant: "destructive",
-                          //     title: "Cannot save your profile",
-                          //     description:
-                          //       "Please make sure all the required fields are satisfied.",
-                          //     duration: 4000,
-                          //   });
-                          // }
-                        }}
-                        disabled={isSaving}
-                        type={"submit"}
-                        className={cn(
-                          "text-xs rounded-2xl h-max",
-                          "hover:bg-green-400/80 bg-green-400",
-                        )}
-                      >
-                        <p>Save</p>
-                      </Button>
-                      <Button
-                        type={"button"}
-                        onClick={() => toggleEditMode()}
-                        className={cn(
-                          "text-xs rounded-2xl h-max",
-                          "text-[#727272] bg-[#E8ECEF] hover:bg-[#E8ECEF]/80",
-                        )}
-                      >
-                        <p>Cancel</p>
-                      </Button>
-                    </div>
+                  {userId !== user!.member_id.toString() && (
+                    <Button
+                      disabled={!selectedConversation}
+                      type="button"
+                      className="text-xs border-primary hover:bg-primary px-2 py-1"
+                    >
+                      <Link className="flex" href="/messages">
+                        <p className="flex">
+                          Chat
+                          <span>
+                            <MessageCircleIcon className="h-4" />
+                          </span>
+                        </p>
+                      </Link>
+                    </Button>
                   )}
-                  {!isEditing && (
+                </div>
+              )}
+              {isEditing && (
+                <FormField
+                  name="nickname"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormLabel className="text-primary">
+                          Nickname
+                        </FormLabel>
+                        <Input
+                          placeholder="Enter nickname"
+                          type="text"
+                          defaultValue={field.value}
+                          onChange={field.onChange}
+                          className="outline-0 border border rounded-lg w-full py-3 px-5"
+                          name="nickname"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              )}
+            </div>
+            {userId === user!.member_id.toString() && (
+              <>
+                {isEditing && (
+                  <div className="flex space-x-2">
+                    <Button
+                      onClick={!formState.isDirty ? () => {} : () => {
+                        // if (isEditing && !formState.isValid) {
+                        //   toast({
+                        //     variant: "destructive",
+                        //     title: "Cannot save your profile",
+                        //     description:
+                        //       "Please make sure all the required fields are satisfied.",
+                        //     duration: 4000,
+                        //   });
+                        // }
+                      }}
+                      disabled={isSaving}
+                      type={"submit"}
+                      className={cn(
+                        "text-xs rounded-2xl h-max",
+                        "hover:bg-green-400/80 bg-green-400",
+                      )}
+                    >
+                      <p>Save</p>
+                    </Button>
                     <Button
                       type={"button"}
-                      onClick={() => {
-                        if (!isEditing) {
-                          toggleEditMode();
-                        }
-                      }}
+                      onClick={() => toggleEditMode()}
                       className={cn(
                         "text-xs rounded-2xl h-max",
                         "text-[#727272] bg-[#E8ECEF] hover:bg-[#E8ECEF]/80",
                       )}
                     >
-                      <>
-                        <Pencil className="h-4 mr-2" />
-                        <span>Edit Profile</span>
-                      </>
+                      <p>Cancel</p>
                     </Button>
-                  )}
-                </>
-              )}
-            </div>
-            {!isEditing && user!.is_active && (
-              <div className="h-full hidden lg:block">
-                <div className="pt-5 flex w-full justify-start items-start flex-wrap text-xs space-x-2">
-                  {headerValues.height && (
-                    <p className="rounded-md w-max h-max bg-[#FFF2F7] text-[#FF599B] p-2 mb-2">
-                      {`${headerValues.height} cm`}
-                    </p>
-                  )}
-                  {headerValues.gender && (
-                    <p className="rounded-md w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
-                      {headerValues.gender == "F" && "Female"}
-                      {headerValues.gender == "M" && "Male"}
-                    </p>
-                  )}
-                  {headerValues.maritalStatus && (
-                    <p className="rounded-md w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
-                      {headerValues.maritalStatus}
-                    </p>
-                  )}
-                  {headerValues.country_name && (
-                    <p className="rounded-full w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
-                      {headerValues.country_name}
-                    </p>
-                  )}
-                  {headerValues.occupation_title && (
-                    <p className="rounded-full w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
-                      {headerValues.occupation_title}
-                    </p>
-                  )}
-                </div>
-              </div>
+                  </div>
+                )}
+                {!isEditing && (
+                  <Button
+                    type={"button"}
+                    onClick={() => {
+                      if (!isEditing) {
+                        toggleEditMode();
+                      }
+                    }}
+                    className={cn(
+                      "text-xs rounded-2xl h-max",
+                      "text-[#727272] bg-[#E8ECEF] hover:bg-[#E8ECEF]/80",
+                    )}
+                  >
+                    <>
+                      <Pencil className="h-4 mr-2" />
+                      <span>Edit Profile</span>
+                    </>
+                  </Button>
+                )}
+              </>
             )}
           </div>
+          {!isEditing && user!.is_active && (
+            <div className="h-full hidden lg:block">
+              <div className="pt-5 flex w-full justify-start items-start flex-wrap text-xs space-x-2">
+                {headerValues.height && (
+                  <p className="rounded-md w-max h-max bg-[#FFF2F7] text-[#FF599B] p-2 mb-2">
+                    {`${headerValues.height} cm`}
+                  </p>
+                )}
+                {headerValues.gender && (
+                  <p className="rounded-md w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
+                    {headerValues.gender == "F" && "Female"}
+                    {headerValues.gender == "M" && "Male"}
+                  </p>
+                )}
+                {headerValues.maritalStatus && (
+                  <p className="rounded-md w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
+                    {headerValues.maritalStatus}
+                  </p>
+                )}
+                {headerValues.country_name && (
+                  <p className="rounded-full w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
+                    {headerValues.country_name}
+                  </p>
+                )}
+                {headerValues.occupation_title && (
+                  <p className="rounded-full w-max h-max bg-[#FFF2F7] text-[#FF599B] px-5 py-1 mb-2">
+                    {headerValues.occupation_title}
+                  </p>
+                )}
+              </div>
+            </div>
+          )}
         </div>
       </div>
-    </Dialog>
+    </div>
   );
 };
 
