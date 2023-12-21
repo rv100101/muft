72e87b423eef280b85ce039462f170527f12f205
lovenangelo <lovenangelo.dev@gmail.com>
2023-12-21 08:42:28 +0800

    feat: added sentry

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import pageRoutes, {
 import { useUserStore } from "./zustand/auth/user";
 import { cn } from "./lib/utils";
 import ViewUser from "./components/profile/viewUser";
+import { ErrorBoundary } from "@sentry/react";
 // import { useEffect } from "react";
 // import runOneSignal from "./lib/oneSignal";
 
@@ -22,106 +23,108 @@ function App() {
   // });
 
   return (
-    <div
-      className={cn(
-        location == "/auth/signin" ? "h-screen flex flex-col" : "h-full"
-      )}
-    >
-      <div className="h-max">
-        {routesWithFooterAndTopNav.includes(location) && !user && <TopNav />}
-      </div>
-      <Route
-        path="/"
-        component={
-          user
-            ? pageRoutes.homePage.component
-            : pageRoutes.landingPage.component
-        }
-      />
-      <div className="flex-auto h-full">
-        <Route
-          path={pageRoutes.signUp.path}
-          component={pageRoutes.signUp.component}
-        />
-        <Route
-          path={pageRoutes.signIn.path}
-          component={pageRoutes.signIn.component}
-        />
-      </div>
-
-      <div className="md:mx-12 lg:mx-36">
-        <Route
-          path={pageRoutes.privacyPolicyPage.path}
-          component={pageRoutes.privacyPolicyPage.component}
-        />
-        <Route
-          path={pageRoutes.termsPage.path}
-          component={pageRoutes.termsPage.component}
-        />
-        <Route
-          path={pageRoutes.releaseNotesPage.path}
-          component={pageRoutes.releaseNotesPage.component}
-        />
-      </div>
-
-      <Route
-        path={pageRoutes.about.path}
-        component={pageRoutes.about.component}
-      />
-
-      <Route
-        path={pageRoutes.subscription.path}
-        component={pageRoutes.subscription.component}
-      />
-
-      {/* Authenticated routes */}
-      {user && (
-        <>
-          <Route
-            path={pageRoutes.messagingPage.path}
-            component={pageRoutes.messagingPage.component}
-          />
-          <Route
-            path={pageRoutes.notificationsPage.path}
-            component={pageRoutes.notificationsPage.component}
-          />
-          <Route path={"/profile/:id"}>
-            {(params) => <ViewUser id={params.id} />}
-          </Route>
-          <Route
-            path={pageRoutes.likes.path}
-            component={pageRoutes.likes.component}
-          />
-          <Route
-            path={pageRoutes.Favorites.path}
-            component={pageRoutes.Favorites.component}
-          />
-          <Route
-            path={pageRoutes.settings.path}
-            component={pageRoutes.settings.component}
-          />
-          <Route path="/members/:id">
-            {(params) => <ViewUser id={params.id} />}
-          </Route>
-        </>
-      )}
-
-      {!user && !noUserOnlyRoutes.includes(location) && location !== "/" && (
-        <Redirect to="/auth/signin" />
-      )}
-
-      {user && noUserOnlyRoutes.includes(location) && <Redirect to="/" />}
-
-      {user && (!user.profile_completed || !user.is_active) && (
-        <Redirect to={`/profile/${user.member_id}`} />
-      )}
-
-      {routesWithFooterAndTopNav.includes(location) && !user && (
-        <div className="bg-[#0C1223] h-max">
-          <Footer />
+    <ErrorBoundary>
+      <div
+        className={cn(
+          location == "/auth/signin" ? "h-screen flex flex-col" : "h-full"
+        )}
+      >
+        <div className="h-max">
+          {routesWithFooterAndTopNav.includes(location) && !user && <TopNav />}
         </div>
-      )}
-    </div>
+        <Route
+          path="/"
+          component={
+            user
+              ? pageRoutes.homePage.component
+              : pageRoutes.landingPage.component
+          }
+        />
+        <div className="flex-auto h-full">
+          <Route
+            path={pageRoutes.signUp.path}
+            component={pageRoutes.signUp.component}
+          />
+          <Route
+            path={pageRoutes.signIn.path}
+            component={pageRoutes.signIn.component}
+          />
+        </div>
+
+        <div className="md:mx-12 lg:mx-36">
+          <Route
+            path={pageRoutes.privacyPolicyPage.path}
+            component={pageRoutes.privacyPolicyPage.component}
+          />
+          <Route
+            path={pageRoutes.termsPage.path}
+            component={pageRoutes.termsPage.component}
+          />
+          <Route
+            path={pageRoutes.releaseNotesPage.path}
+            component={pageRoutes.releaseNotesPage.component}
+          />
+        </div>
+
+        <Route
+          path={pageRoutes.about.path}
+          component={pageRoutes.about.component}
+        />
+
+        <Route
+          path={pageRoutes.subscription.path}
+          component={pageRoutes.subscription.component}
+        />
+
+        {/* Authenticated routes */}
+        {user && (
+          <>
+            <Route
+              path={pageRoutes.messagingPage.path}
+              component={pageRoutes.messagingPage.component}
+            />
+            <Route
+              path={pageRoutes.notificationsPage.path}
+              component={pageRoutes.notificationsPage.component}
+            />
+            <Route path={"/profile/:id"}>
+              {(params) => <ViewUser id={params.id} />}
+            </Route>
+            <Route
+              path={pageRoutes.likes.path}
+              component={pageRoutes.likes.component}
+            />
+            <Route
+              path={pageRoutes.Favorites.path}
+              component={pageRoutes.Favorites.component}
+            />
+            <Route
+              path={pageRoutes.settings.path}
+              component={pageRoutes.settings.component}
+            />
+            <Route path="/members/:id">
+              {(params) => <ViewUser id={params.id} />}
+            </Route>
+          </>
+        )}
+
+        {!user && !noUserOnlyRoutes.includes(location) && location !== "/" && (
+          <Redirect to="/auth/signin" />
+        )}
+
+        {user && noUserOnlyRoutes.includes(location) && <Redirect to="/" />}
+
+        {user && (!user.profile_completed || !user.is_active) && (
+          <Redirect to={`/profile/${user.member_id}`} />
+        )}
+
+        {routesWithFooterAndTopNav.includes(location) && !user && (
+          <div className="bg-[#0C1223] h-max">
+            <Footer />
+          </div>
+        )}
+      </div>
+    </ErrorBoundary>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,23 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://fd6a643efc57d33d656c0a08f965cbbf@o1300880.ingest.sentry.io/4506415538765824",
+  integrations: [
+    new Sentry.BrowserTracing(),
+    new Sentry.Replay(),
+  ],
+
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
+});
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
