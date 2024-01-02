67f9ef9ceebf1706848d8a78193ebb513550dffa
lovenangelo <lovenangelo.dev@gmail.com>
2024-01-02 11:25:18 +0800

    fix: index of mapped elements

--- src/components/homeSearchUsersInput.tsx
+++ src/components/homeSearchUsersInput.tsx
@@ -69,9 +69,10 @@ const HomepageSearchInput = () => {
       {debouncedSearchValue.length > 0 ? (
         searchResults.length !== 0 ? (
           <div className="mt-1 sticky z-40 h-min max-h-[200px] border rounded-b-md lg:w-[330px] bg-white overflow-y-auto">
-            {searchResults.map((result: SearchResultItem) => {
+            {searchResults.map((result: SearchResultItem, index: number) => {
               return (
                 <Button
+                  key={index}
                   onClick={() => {
                     searchClickHandler(result!.member_id!);
                   }}
