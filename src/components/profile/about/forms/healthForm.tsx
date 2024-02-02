c0b82ff43e5721afbd0bc676db89a92cbc4a1724
Loven Angelo <lovenangelo.dev@gmail.com>
2024-02-02 10:36:34 +0800

    fix: rtl of select fields

--- src/components/profile/about/forms/healthForm.tsx
+++ src/components/profile/about/forms/healthForm.tsx
@@ -62,7 +62,9 @@ const HealthForm = () => {
                       defaultValue={field.value}
                     >
                       <FormControl>
-                        <SelectTrigger>
+                        <SelectTrigger
+                          dir={i18n.language == 'ar' ? "rtl" : "ltr"}
+                        >
                           <SelectValue
                             placeholder={i18n.language == 'en' ? "Do you workout?" : "يرجى الاختيار"}
                           />
