8217e8dd1885238f9ee72c4871c2538e51c61ac1
Loven Angelo <lovenangelo.dev@gmail.com>
2024-02-02 11:10:33 +0800

    feat: hide error on valid values

--- src/components/onboarding/BasicInformationStep.tsx
+++ src/components/onboarding/BasicInformationStep.tsx
@@ -4,14 +4,31 @@ import { useQuery } from "@tanstack/react-query";
 import selectOptions from "@/zustand/profile/selectData/selectOptions";
 import profileContentQuery from "@/queries/profile/profileContent";
 import { Nationality } from "@/types/profile";
+import { useFormContext, useWatch } from "react-hook-form";
+import { useEffect } from "react";
+import { fieldNames } from "@/lib/formFieldKeys";
+import onboardingStore from "@/zustand/profile/onboarding/onboardingStore";
 
 
 const BasicInformationStep = () => {
   const [, i18n] = useTranslation()
-  // const user = useUserStore(state => state.user);
+  const {
+    trigger
+  } = useFormContext();
+
   const {
     setNationalities
   } = selectOptions();
+
+  const basicInfoValues = useWatch({ name: fieldNames[0] });
+  const step = onboardingStore(state => state.step);
+
+  useEffect(() => {
+    if (step == 1) {
+      trigger(fieldNames[0])
+    }
+  }, [basicInfoValues, trigger, step]);
+
   useQuery({
     queryFn: () => profileContentQuery.editOptions.getNationality(i18n.language),
     refetchInterval: Infinity,
