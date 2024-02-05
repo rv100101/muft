c8d23e200d8aa2ce254f75180b5841480612d93f
lovenangelo <lovenangelo.dev@gmail.com>
2024-02-05 08:40:02 +0800

    feat: animatino on save

--- src/components/onboarding/OnboardingWrapper.tsx
+++ src/components/onboarding/OnboardingWrapper.tsx
@@ -1,36 +1,38 @@
-import onboardingStore from "@/zustand/profile/onboarding/onboardingStore"
-import { Button } from "../ui/button"
-import { Progress } from "../ui/progress"
-import BasicInformationStep from "./BasicInformationStep"
-import { cn } from "@/lib/utils"
-import { FieldErrors, FieldValues, useFormContext } from "react-hook-form"
-import { fieldNames } from "@/lib/formFieldKeys"
-import { useCallback, useEffect, useState, } from "react"
-import LocationStep from "./LocationStep"
-import BackgroundStep from "./BackgroundStep"
-import LanguagesStep from "./LanguagesStep"
-import AppearanceStep from "./AppearanceStep"
-import LifestyleStep from "./LifestyleStep"
-import PetsStep from "./PetsStep"
-import FavoriteFoodStep from "./FavoriteFoodStep"
-import HealthStep from "./HealthStep"
-import MaritalStatusStep from "./MaritalStatusStep"
-import EmploymentStatusStep from "./EmploymentStatusStep"
-import InterestsStep from "./InterestsStep"
-import profileAboutContentStore from "@/zustand/profile/profileAboutStore"
-import { Loader2 } from "lucide-react"
-import { useTranslation } from "react-i18next"
-import PreferredLanguageDialog from "../preferredLanguageDialog"
+import onboardingStore from "@/zustand/profile/onboarding/onboardingStore";
+import { Button } from "../ui/button";
+import { Progress } from "../ui/progress";
+import BasicInformationStep from "./BasicInformationStep";
+import { cn } from "@/lib/utils";
+import { FieldErrors, FieldValues, useFormContext } from "react-hook-form";
+import { fieldNames } from "@/lib/formFieldKeys";
+import { useCallback, useEffect, useState } from "react";
+import LocationStep from "./LocationStep";
+import BackgroundStep from "./BackgroundStep";
+import LanguagesStep from "./LanguagesStep";
+import AppearanceStep from "./AppearanceStep";
+import LifestyleStep from "./LifestyleStep";
+import PetsStep from "./PetsStep";
+import FavoriteFoodStep from "./FavoriteFoodStep";
+import HealthStep from "./HealthStep";
+import MaritalStatusStep from "./MaritalStatusStep";
+import EmploymentStatusStep from "./EmploymentStatusStep";
+import InterestsStep from "./InterestsStep";
+import profileAboutContentStore from "@/zustand/profile/profileAboutStore";
+import { Loader2 } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import PreferredLanguageDialog from "../preferredLanguageDialog";
+import logo from "@/assets/single-logo.png";
 
 const OnboardingWrapper = () => {
-  const [t, i18n] = useTranslation(); const { isSaving
-  } = profileAboutContentStore();
+  const [t, i18n] = useTranslation();
+  const { isSaving } = profileAboutContentStore();
   const {
-    formState: { errors, dirtyFields, isValidating, }, trigger
+    formState: { errors, dirtyFields, isValidating },
+    trigger,
   } = useFormContext();
 
-  const step = onboardingStore(state => state.step);
-  const setStep = onboardingStore(state => state.setStep);
+  const step = onboardingStore((state) => state.step);
+  const setStep = onboardingStore((state) => state.setStep);
   const [goNext, setGoNext] = useState(false);
   const [submit, setSubmit] = useState(false);
 
@@ -40,9 +42,12 @@ const OnboardingWrapper = () => {
     }
   }, [step, setStep]);
 
-  function checkKeysInErrorObject(keys: string[], errorObj: FieldErrors<FieldValues>): boolean {
+  function checkKeysInErrorObject(
+    keys: string[],
+    errorObj: FieldErrors<FieldValues>
+  ): boolean {
     for (const key of keys) {
-      if ((key in errorObj)) {
+      if (key in errorObj) {
         console.log(key);
         return false;
       }
@@ -50,7 +55,10 @@ const OnboardingWrapper = () => {
     return true;
   }
 
-  function checkDirtyFields(keys: string[], errorObj: Record<string, boolean>): boolean {
+  function checkDirtyFields(
+    keys: string[],
+    errorObj: Record<string, boolean>
+  ): boolean {
     for (const key of keys) {
       if (!(key in errorObj)) {
         return false;
@@ -62,17 +70,14 @@ const OnboardingWrapper = () => {
   useEffect(() => {
     const getFields: () => string[] = () => {
       if (step >= fieldNames.length) {
-        return fieldNames[fieldNames.length - 1]
+        return fieldNames[fieldNames.length - 1];
       } else {
-        return fieldNames[step - 1]
+        return fieldNames[step - 1];
       }
-    }
+    };
     const validateFields = getFields();
     const pass = checkKeysInErrorObject(validateFields, errors);
     const isDirty = checkDirtyFields(validateFields, dirtyFields);
-
-    console.log(dirtyFields);
-    console.log(!isValidating, pass, isDirty, goNext);
 
     if (!isValidating && pass && isDirty && goNext) {
       setGoNext(false);
@@ -81,26 +86,34 @@ const OnboardingWrapper = () => {
       setGoNext(false);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dirtyFields, errors, goNext, isValidating, setStep, trigger])
-
+  }, [dirtyFields, errors, goNext, isValidating, setStep, trigger]);
 
   const handleNext = useCallback(() => {
     const getFields: () => string[] = () => {
       if (step >= fieldNames.length) {
-        return fieldNames[fieldNames.length - 1]
+        return fieldNames[fieldNames.length - 1];
       } else {
-        return fieldNames[step - 1]
+        return fieldNames[step - 1];
       }
-    }
+    };
     const validateFields = getFields();
     console.log("FIELDS TO VALIDATE: ", validateFields);
     trigger(validateFields);
     setGoNext(true);
   }, [step, trigger]);
 
-  return (
+  return isSaving ? (
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <img src={logo} className="animate-bounce" alt="muffin-logo" />
+      <SavingAnimationMessage step={1} />
+    </div>
+  ) : (
     <>
-      <div className={cn("my-8 text-lg flex px-8 sm:w-1/2 items-center justify-between")}>
+      <div
+        className={cn(
+          "my-8 text-lg flex px-8 sm:w-1/2 items-center justify-between"
+        )}
+      >
         <StepHeader step={step} />
         <PreferredLanguageDialog
           showTrigger={true}
@@ -109,57 +122,92 @@ const OnboardingWrapper = () => {
         />
       </div>
       <div className="px-12 w-full flex justify-center">
-        <Progress value={(step / 12) * 100} className={cn("sm:w-1/2 mb-8 h-2", i18n.language == 'ar' && 'rotate-180')} />
+        <Progress
+          value={(step / 12) * 100}
+          className={cn(
+            "sm:w-1/2 mb-8 h-2",
+            i18n.language == "ar" && "rotate-180"
+          )}
+        />
       </div>
       <StepView step={step} />
       <hr className="h-4 w-full sm:w-1/2 mt-8" />
-      <div className={cn("w-full px-8 sm:w-1/2 flex justify-end", step !== 1 && "justify-between")}>
-        {
-          step !== 1 && <Button
+      <div
+        className={cn(
+          "w-full px-8 sm:w-1/2 flex justify-end",
+          step !== 1 && "justify-between"
+        )}
+      >
+        {step !== 1 && (
+          <Button
             disabled={isSaving}
-            type="button" className="hover:bg-[#FF599B]/90" onClick={() => {
-              setStep(step - 1)
+            type="button"
+            className="hover:bg-[#FF599B]/90"
+            onClick={() => {
+              setStep(step - 1);
               setGoNext(false);
-            }}>{t("onboarding.back")}</Button>
-        }
-        {
-          step !== 12 ? <Button type="button" className="hover:bg-[#FF599B]/90" onClick={handleNext} >{t("onboarding.next")}</Button>
-            : <Button className=" hover:bg-[#FF599B]/90"
-              disabled={isSaving}
-              onClick={() => {
-                setSubmit(true);
-              }}
-              type={submit ? "submit" : "button"}
-            >Finish
-              {isSaving && (
-                <span>
-                  <Loader2 className="ml-2 h-4 w-4 animate-spin" />
-                </span>
-              )}
-            </Button>
-        }
-      </div >
+            }}
+          >
+            {t("onboarding.back")}
+          </Button>
+        )}
+        {step !== 12 ? (
+          <Button
+            type="button"
+            className="hover:bg-[#FF599B]/90"
+            onClick={handleNext}
+          >
+            {t("onboarding.next")}
+          </Button>
+        ) : (
+          <Button
+            className=" hover:bg-[#FF599B]/90"
+            disabled={isSaving}
+            onClick={() => {
+              setSubmit(true);
+            }}
+            type={submit ? "submit" : "button"}
+          >
+            Finish
+            {isSaving && (
+              <span>
+                <Loader2 className="ml-2 h-4 w-4 animate-spin" />
+              </span>
+            )}
+          </Button>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
 const StepView = ({ step }: { step: number }) => {
   const forms = [
-    <BasicInformationStep />, <LocationStep />, <BackgroundStep />, <LanguagesStep />, <AppearanceStep />, <LifestyleStep />, <PetsStep />,
-    <FavoriteFoodStep />, <HealthStep />, <MaritalStatusStep />, <EmploymentStatusStep />, <InterestsStep />
+    <BasicInformationStep />,
+    <LocationStep />,
+    <BackgroundStep />,
+    <LanguagesStep />,
+    <AppearanceStep />,
+    <LifestyleStep />,
+    <PetsStep />,
+    <FavoriteFoodStep />,
+    <HealthStep />,
+    <MaritalStatusStep />,
+    <EmploymentStatusStep />,
+    <InterestsStep />,
   ];
   if (step > forms.length) {
     return forms[forms.length - 1];
   }
   return forms[step - 1];
-}
+};
 
 const StepHeader = ({ step }: { step: number }) => {
   const [t] = useTranslation();
   const headers = [
     <h1 className="font-semibold ">{t("onboarding.startWithBasics")} ✅</h1>,
     <h1 className="font-semibold">{t("onboarding.whereAreYouFrom")} 🗺️</h1>,
-    <h1 className="font-semibold">{t("onboarding.tellBackground")}🏫</h1>,
+    <h1 className="font-semibold">{t("onboarding.tellBackground")} 🏫</h1>,
     <h1 className="font-semibold">{t("memberDetails.languages")} 🦜</h1>,
     <h1 className="font-semibold">{t("memberDetails.appearance")} ✨</h1>,
     <h1 className="font-semibold">{t("memberDetails.lifestyle")} ☀️</h1>,
@@ -174,6 +222,16 @@ const StepHeader = ({ step }: { step: number }) => {
     return headers[headers.length - 1];
   }
   return headers[step - 1];
-}
+};
 
-export default OnboardingWrapper
+const SavingAnimationMessage = ({ step }: { step: number }) => {
+  const headers = [
+    <p className="font-semibold text-2xl">Saving your profile... 📋</p>,
+  ];
+  if (step > headers.length) {
+    return headers[headers.length - 1];
+  }
+  return headers[step - 1];
+};
+
+export default OnboardingWrapper;
