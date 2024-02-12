acd464c3bb6fde982f2589d2a6902ad3d712b9f4
Loven Angelo <lovenangelo.dev@gmail.com>
2024-02-12 10:05:09 +0800

    fix: notif switch

--- src/components/settings/notification/pushNotifcationContent.tsx
+++ src/components/settings/notification/pushNotifcationContent.tsx
@@ -2,11 +2,20 @@
 // import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Switch } from "@/components/ui/switch";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import OneSignal from "react-onesignal";
 
 const PushNotifcationContent = () => {
   const [t, i18n] = useTranslation();
+  const [notificationEnabled, setNotificationEnabled] = useState(
+    OneSignal.User.PushSubscription.optedIn
+  );
+  useEffect(() => {
+    setNotificationEnabled(
+      OneSignal.User.PushSubscription.optedIn
+    )
+  }, []);
   return (
     <div className="flex flex-col border-b w-full justify-center text-[#727272] space-y-5 px-5 py-10">
       <p className="font-semibold text-lg">{t("settings.notifications")}</p>
@@ -16,52 +25,17 @@ const PushNotifcationContent = () => {
           {t("settings.enablePushNotifications")}
         </p>
         <Switch
+          dir="ltr"
           className={cn(i18n.language == "ar" && "rotate-180")}
           id="airplane-mode"
-          checked={OneSignal.User.PushSubscription.optedIn}
+          checked={notificationEnabled}
           onCheckedChange={(checked) => {
             checked
               ? OneSignal.User.PushSubscription.optIn()
               : OneSignal.User.PushSubscription.optOut();
           }}
         />
-        {/* <Label htmlFor="airplane-mode">Airplane Mode</Label> */}
       </div>
-      {/* <div className="flex flex-col space-y-3  border-b py-5">
-        <p className="font-medium text-md">Messages</p>
-        <RadioGroup defaultValue="comfortable">
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="default" id="r1" />
-            <Label htmlFor="r1">Off</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="comfortable" id="r2" />
-            <Label htmlFor="r2">From profiles I follow</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="compact" id="r3" />
-            <Label htmlFor="r3">From Everyone</Label>
-          </div>
-        </RadioGroup>
-      </div> */}
-
-      {/* <div className="flex flex-col space-y-3">
-        <p className="font-medium text-md mt-5">Follower Request</p>
-        <RadioGroup defaultValue="comfortable">
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="default" id="r1" />
-            <Label htmlFor="r1">Off</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="comfortable" id="r2" />
-            <Label htmlFor="r2">From profiles I follow</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="compact" id="r3" />
-            <Label htmlFor="r3">From Everyone</Label>
-          </div>
-        </RadioGroup>
-      </div> */}
     </div>
   );
 };
