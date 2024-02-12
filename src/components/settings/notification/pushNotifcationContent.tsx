78883ad445fb610ccdcda58b7f60d252c6f5005f
Loven Angelo <lovenangelo.dev@gmail.com>
2024-02-12 10:17:00 +0800

    refactor: add safari and clean up

--- src/components/settings/notification/pushNotifcationContent.tsx
+++ src/components/settings/notification/pushNotifcationContent.tsx
@@ -1,5 +1,3 @@
-// import { Label } from "@/components/ui/label";
-// import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Switch } from "@/components/ui/switch";
 import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
--- src/lib/oneSignal.ts
+++ src/lib/oneSignal.ts
@@ -3,6 +3,7 @@ import OneSignal from "react-onesignal";
 export default async function runOneSignal() {
   await OneSignal.init({
     appId: "c5ca18a9-5d6b-479c-8f1b-71ddc4ab5694",
+    safari_web_id: "web.onesignal.auto.27d2eba6-7621-43e8-b8d4-d2a9de3b8fea",
     notifyButton: {
       enable: true,
     },
