083f422e0ae56f8a8666ed636f192c464350e2bf
lovenangelo <lovenangelo.dev@gmail.com>
2024-02-13 16:07:52 +0800

    fix: loading

--- src/components/home/posts.tsx
+++ src/components/home/posts.tsx
@@ -41,7 +41,7 @@ const Posts = ({
       {isLoading ? (
         <div className="no-scrollbar flex flex-col items-center lg:p-5 px-0 lg:w-[500px] w-screen h-screen sm:w-full rounded-b-xl space-y-4 border-x border-[#E0E0E0] dark:border-[#131d2d] lg:h-min overflow-y-auto">
           {/* <div className="flex flex-col justify-center space-x-4 w-full ml-5 mt-10 border w-full"> */}
-          <div className="flex flex-col items-center space-y-2 py-4 px-10 border-x bg-white m-5 lg:w-[500px] w-[350px] dark:bg-[#334155]">
+          <div className="flex flex-col items-center space-y-2 py-4 px-10 lg:border-x bg-white m-5 lg:w-[500px] w-[350px] dark:bg-[#334155]">
             <Skeleton className="h-[500px] w-full" />
           </div>
           <div className="flex flex-col items-center space-y-2 p-5 border bg-white m-5 lg:w-[500px] w-[350px] dark:bg-[#334155]">
