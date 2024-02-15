7a2867fdc7cb9cb2e92f653f283fd885432c3d6b
lovenangelo <lovenangelo.dev@gmail.com>
2024-02-15 14:26:46 +0800

    fix: messages

--- src/components/messaging/chatConversation.tsx
+++ src/components/messaging/chatConversation.tsx
@@ -5,12 +5,14 @@ import ChatInput from "./chatInput";
 
 const ChatConversation = () => {
   return (
-    <div className="sm:border-l w-full h-full flex flex-col dark:text-white ">
-      <MessageHeader />
-      <div className="h-[calc(100vh-164px)]">
+    <div className="sm:border-l w-full h-screen flex flex-col dark:text-white">
+      <div className="min-h-min">
+        <MessageHeader />
+      </div>
+      <div className="h-[calc(100vh-164px)] md:flex-1 md:h-full overflow-y-auto">
         <ChatMessages />
       </div>
-      <div className="fixed sm:static bottom-0 h-30">
+      <div className="fixed w-full sm:static bottom-0">
         <ChatInput />
       </div>
     </div>
