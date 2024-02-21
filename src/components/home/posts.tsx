f26df96a9475ac01ff7e7d9464c5b7ba3dbedc87
lovenangelo <lovenangelo.dev@gmail.com>
2024-02-21 13:08:12 +0800

    fix: skel loader width

--- src/components/home/posts.tsx
+++ src/components/home/posts.tsx
@@ -38,7 +38,7 @@ const Posts = ({
       className="col-span-4 overflow-y-auto w-full lg:w-min no-scrollbar 2xl:w-1/2"
     >
       {isLoading ? (
-        <div className="no-scrollbar flex flex-col items-center lg:p-5 px-0 lg:w-[500px] w-screen h-screen sm:w-full rounded-b-xl space-y-4 border-x border-[#E0E0E0] dark:border-[#131d2d] lg:h-min overflow-y-auto">
+        <div className="no-scrollbar flex flex-col items-center lg:p-5 px-0 lg:w-[468px] w-screen h-screen sm:w-full rounded-b-xl space-y-4 border-x border-[#E0E0E0] dark:border-[#131d2d] lg:h-min overflow-y-auto">
           {/* <div className="flex flex-col justify-center space-x-4 w-full ml-5 mt-10 border w-full"> */}
           <div className="flex flex-col items-center space-y-2 py-4 px-10 lg:border-x bg-white m-5 lg:w-[500px] w-[350px] dark:bg-[#334155]">
             <Skeleton className="h-[500px] w-full" />
