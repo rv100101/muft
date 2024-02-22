a85bd3423bc9bfaf00297a7a9d5e2c1989ba70b5
lovenangelo <lovenangelo.dev@gmail.com>
2024-02-22 15:29:30 +0800

    fix: fix header

--- src/components/messaging/chatConversation.tsx
+++ src/components/messaging/chatConversation.tsx
@@ -6,7 +6,7 @@ import ChatInput from "./chatInput";
 const ChatConversation = () => {
   return (
     <div className="sm:border-l w-full h-screen flex flex-col dark:text-white">
-      <div className="min-h-min flex-shrink-0">
+      <div className="fix top-0 min-h-min h-[64px] flex-shrink-0">
         <MessageHeader />
       </div>
       <div className=" flex-grow h-max md:flex-1 md:h-full overflow-y-auto">
