79c5c127e86f63ef9f7b7dc91fb2631b7adec673
lovenangelo <lovenangelo.dev@gmail.com>
2024-03-04 13:20:13 +0800

    fix: horizontal scroll

--- src/components/home/list.tsx
+++ src/components/home/list.tsx
@@ -42,7 +42,7 @@ const MemberList = ({
     <div
       onScroll={handleScroll}
       ref={containerRef}
-      className="no-scrollbar pt-8 py-4 flex flex-col items-center lg:p-8 px-0 md:w-full w-screen h-screen sm:w-full rounded-b-xl space-y-4 border-[#E0E0E0] dark:border-[#131d2d] overflow-y-scroll">
+      className="no-scrollbar pt-8 py-4 flex flex-col items-center lg:p-8 px-0 md:w-full w-screen h-screen sm:w-full rounded-b-xl space-y-4 border-[#E0E0E0] dark:border-[#131d2d] overflow-y-scroll overflow-x-clip">
       {memberList.length > 0 ? (
         memberList.map((post, index: number) => (
           <PostItem key={index} memberData={post} />
