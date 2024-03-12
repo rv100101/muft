d4f77a9669619b3f638a33ac112d2b7b26b5a995
lovenangelo <lovenangelo.dev@gmail.com>
2024-03-12 09:13:40 +0800

    refactor: new app id and safari id for onesignal

--- src/lib/oneSignal.ts
+++ src/lib/oneSignal.ts
@@ -12,23 +12,9 @@ export default async function runOneSignal() {
 }
 
 const getAppId: () => string = () => {
-  const base_url = window.location.origin;
-  if (base_url == "https://www.muffin.ph") {
-    return "c5ca18a9-5d6b-479c-8f1b-71ddc4ab5694";
-  } else if (base_url == "https://www.muffin.ae") {
-    return "0014a853-58bd-41a5-894b-67e8d95863f5";
-  } else {
-    return "";
-  }
+  return "c5ca18a9-5d6b-479c-8f1b-71ddc4ab5694";
 };
 
 const getSafariId: () => string = () => {
-  const base_url = window.location.origin;
-  if (base_url == "https://www.muffin.ph") {
-    return "web.onesignal.auto.27d2eba6-7621-43e8-b8d4-d2a9de3b8fea";
-  } else if (base_url == "https://www.muffin.ae") {
-    return "web.onesignal.auto.0a199198-d5df-41c5-963c-72a0258657aa";
-  } else {
-    return "";
-  }
+  return "web.onesignal.auto.27d2eba6-7621-43e8-b8d4-d2a9de3b8fea";
 };
