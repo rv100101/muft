adec16fe7bad3070b3ec18f91a4ae136e6c7934d
lovenangelo <lovenangelo.dev@gmail.com>
2024-03-12 09:57:50 +0800

    refactor: update sentry

--- src/main.tsx
+++ src/main.tsx
@@ -33,15 +33,19 @@ i18n
 
 Sentry.init({
   dsn: "https://fd6a643efc57d33d656c0a08f965cbbf@o1300880.ingest.sentry.io/4506415538765824",
-  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.07,
+  replaysOnErrorSampleRate: 0.07,
 });
+
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
