80dbb10dd3e7e343442995b7bf5233610184d97e
lovenangelo <lovenangelo.dev@gmail.com>
2024-04-15 09:27:44 +0800

    refactor: update api for search

--- src/components/homeSearchUsersInput.tsx
+++ src/components/homeSearchUsersInput.tsx
@@ -37,7 +37,7 @@ const HomepageSearchInput = () => {
   const debouncedSearchValue = useDebounce(searchValue, 300);
 
   const searchUsers = async () =>
-    await searchQuery.search(debouncedSearchValue, user!.member_id);
+    await searchQuery.search(debouncedSearchValue, user!.member_id, i18n.language);
 
   const { isLoading } = useQuery({
     queryFn: searchUsers,
--- src/queries/search.ts
+++ src/queries/search.ts
@@ -1,12 +1,22 @@
 import axiosQuery from "./axios";
 
-
-const search = async (text: string, member: number) => await axiosQuery.post('/Search', {
-  text, member
-});
+const search = async (text: string, member: number, lang: string) => {
+  const formData = new FormData();
+  formData.append(
+    "auth",
+    "0DB31DEE22DC4C03AD7DAAA9C29518FF3C08D931992A4A5CB0A4FF4CF4707DC6"
+  );
+  formData.append("lang", lang);
+  formData.append("member", member.toString());
+  formData.append("text", text);
+  return await axiosQuery.post(
+    "https://muffinapi.azurewebsites.net/search.php",
+    formData
+  );
+};
 
 const searchQuery = {
-  search
-}
+  search,
+};
 
 export default searchQuery;
