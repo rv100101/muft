a3b124b8f449af0c9b4ca83b72ad232079f9a3b0
Ruel <ruel.velasquez@ziaplex.biz>
2024-08-07 00:30:28 +0800

    locss

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// next.config.js
 import withPlugins from 'next-compose-plugins';
 import MomentLocalesPlugin from 'moment-locales-webpack-plugin';
 
@@ -5,15 +6,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config) => {
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"]
     });
+
+    //
     config.plugins.push(
       new MomentLocalesPlugin({
-        localesToKeep: ['en'], // Specify the locales you need
+        localesToKeep: ['en'], 
       })
     );
+
     return config;
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import * as path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   server: {
     host: '127.0.0.1',
     port: 2930,
     proxy: {
-         '/places': {
+      '/places': {
         target: 'http://127.0.0.1:3000/places',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/places/, ''),
       },
- 
- 
-      // Proxy all static assets
+      // Proxy all Next.js assets
       '/_next': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/_next/, '/_next'),
+      },
+      // Proxy API requests (if needed)
+      '/api': {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
     },
   },
