54d6bab467b9ba3462d37a04276008d52dd25146
Ruel <ruel.velasquez@ziaplex.biz>
2024-08-06 23:11:16 +0800

    locals

--- .next/static/webpack/webpack.6a1f5e08f8526b91.hot-update.js
+++ .next/static/webpack/webpack.6a1f5e08f8526b91.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7ef8d9d4ed1e5b06"; }
+/******/ 	__webpack_require__.h = function() { return "9c51d48fff4a6367"; }
 /******/ }();
 /******/ 
 /******/ }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 import withPlugins from 'next-compose-plugins';
+import MomentLocalesPlugin from 'moment-locales-webpack-plugin';
 
 const nextConfig = {
   reactStrictMode: true,
-    swcMinify: true,
+  swcMinify: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"]
     });
+    config.plugins.push(
+      new MomentLocalesPlugin({
+        localesToKeep: ['en'], // Specify the locales you need
+      })
+    );
     return config;
   },
 };
