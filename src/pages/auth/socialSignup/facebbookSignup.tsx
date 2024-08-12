5d565dd5fa86ea8ce84cb624324756f26cadf9d3
Ruel <ruel.velasquez@ziaplex.biz>
2024-08-13 00:01:31 +0800

    fbfinal5

--- src/pages/auth/socialSignup/facebbookSignup.tsx
+++ src/pages/auth/socialSignup/facebbookSignup.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import FacebookLogin from "@greatsumini/react-facebook-login";
+import FacebookLogin from "@greatsumini/react-facebook-login";
 import fbLogo from "@/assets/auth/facebook-logo.png";
 
 interface GoogleSignUpButtonProps {
@@ -10,15 +10,14 @@ interface GoogleSignUpButtonProps {
     email_service: string;
   }) => void;
 }
-
 const FacebookLoginButton: React.FC<GoogleSignUpButtonProps> = ({
   onSuccess,
 }) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  /* eslint-disable @typescript-eslint/no-explicit-any */
   const handleResponse = (response: any) => {
     if (response.accessToken) {
+      console.log("Login successful:", response);
+
       const { accessToken, userID } = response;
       fetch(
         `https://graph.facebook.com/${userID}?fields=name,email&access_token=${accessToken}`
@@ -26,6 +25,9 @@ const FacebookLoginButton: React.FC<GoogleSignUpButtonProps> = ({
         .then((res) => res.json())
         .then((data) => {
           const { name, email } = data;
+          console.log("Fetched name:", name);
+          console.log("Fetched email:", email);
+
           const [firstName, ...lastNameParts] = name.split(" ");
           const lastName = lastNameParts.join(" ");
           const email_service = "1";
@@ -41,6 +43,15 @@ const FacebookLoginButton: React.FC<GoogleSignUpButtonProps> = ({
 
   return (
     <div>
+      <FacebookLogin
+        appId="460172250155750" // Replace with your Facebook app ID
+        autoLoad={false}
+        fields="name,email,picture"
+        scope="email"
+        onSuccess={handleResponse}
+        onFail={handleResponse} // Use the same handler for failures
+      />
+
       <button
         onClick={() =>
           (window as any).FB.login(handleResponse, {
