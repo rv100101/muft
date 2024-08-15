65041dda2c70f613789bc4e5b4b9bca927db9764
Ruel <ruel.velasquez@ziaplex.biz>
2024-08-15 21:12:09 +0800

    ssr3

--- src/components/profile/viewUser.tsx
+++ src/components/profile/viewUser.tsx
@@ -5,30 +5,32 @@ import { useUserStore } from "@/zustand/auth/user";
 import profileAboutContentStore from "@/zustand/profile/profileAboutStore";
 import { Helmet } from "react-helmet-async";
 import { useLocation } from "wouter";
+
 const ViewUser = ({ id }: { id: string }) => {
   const [location] = useLocation();
   const { data, profileData } = profileAboutContentStore();
   const user = useUserStore((state) => state.user);
+
+  const ogImage = profileData
+    ? getImagePath(
+        profileData!.gallery_uuid,
+        profileData!.gender,
+        profileData!.member_uuid
+      )
+    : "https://muffin.ae/public/logo.png";
+
   return (
     <AuthenticatedLayout>
       <Helmet>
-        {location.startsWith("/members") ? (
-          <>
-            <title>{data?.nickname ?? "muffin"} | Muffin</title>
-            <link
-              rel="canonical"
-              href={`https://${window.location.hostname}${location}`}
-            />
-          </>
-        ) : (
-          <>
-            <title>My Profile</title>
-            <link
-              rel="canonical"
-              href={`https://${window.location.hostname}${location}`}
-            />
-          </>
-        )}
+        <title>
+          {location.startsWith("/members")
+            ? `${data?.nickname ?? "muffin"} | Muffin`
+            : "My Profile"}
+        </title>
+        <link
+          rel="canonical"
+          href={`https://${window.location.hostname}${location}`}
+        />
 
         <meta
           property="og:title"
@@ -36,25 +38,13 @@ const ViewUser = ({ id }: { id: string }) => {
             user !== null ? `${user.first_name}'s Profile` : "Loading..."
           }
         />
-        <meta property="og:description" content={"Muffin member"} />
+        <meta property="og:description" content="Muffin member" />
         <meta property="og:url" content={window.location.href} />
-        <meta
-          property="og:image"
-          content={
-            profileData !== null
-              ? getImagePath(
-                profileData!.gallery_uuid,
-                profileData!.gender,
-                profileData!.member_uuid
-              )
-              : "https://muffin.ae/public/logo.png"
-          }
-        />
-        <meta
-          property="og:image:secure_url"
-          content={"https://muffin.ae/public/logo.png"}
-        />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:image:secure_url" content={ogImage} />
         <meta property="og:image:alt" content="User's profile image" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Helmet>
 
       <div className="w-full">
